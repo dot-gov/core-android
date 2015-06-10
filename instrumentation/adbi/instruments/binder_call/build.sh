842345c33f1b75cb70e246ac9e550e27ccfca299
zad <e.placidi@hackingteam.com>
2015-06-10 14:55:36 +0200

    Start of support for android5

--- instrumentation/adbi/instruments/binder_call/build.sh
+++ instrumentation/adbi/instruments/binder_call/build.sh
@@ -16,9 +16,36 @@
 #       CREATED: 06/03/2015 09:32:52 CET
 #      REVISION:  ---
 #===============================================================================
-rm ./obj ./libs -r
+debug=""
+echo "args $@"
+while getopts ":dDP" opt; do
+  case $opt in
+    P) pie="PIE=1"
+      echo "position Indipendent code ENABLED"
+      ;;
+    d) debug="DEBUG=1"
+      echo "debug is on"
+      ;;
+    D) echo "debug is off"
+      ;;
+    \?) echo "Invalid option -$OPTARG"
+      ;;
+  esac
+done
 
-cd jni
-ndk-build
-cd -
+cdir=$(pwd)
+cd ../base/
+echo cleaning libs..
+rm -r ./obj/* ./libs/*
+ndk_cmd="ndk-build V=1 $debug $pie"
+echo "Building base $ndk_cmd"
+cd jni &&  ndk-build V=1 clean && $ndk_cmd
+cd ${cdir}
+
+echo cleaning libs..
+rm -r ./obj/* ./libs/*
+ndk_cmd="ndk-build V=1 $debug $pie"
+echo "Building hijack $ndk_cmd"
+cd jni &&  ndk-build V=1 clean && $ndk_cmd
+cd ${cdir}
 

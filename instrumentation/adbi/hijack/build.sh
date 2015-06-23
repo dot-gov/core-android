7eceee0c7ed7be29f1cc0a671e88e280e820cec5
zad <e.placidi@hackingteam.com>
2015-06-23 11:27:20 +0200

    re-enabled forcefull rebuild of injected library

--- instrumentation/adbi/hijack/build.sh
+++ instrumentation/adbi/hijack/build.sh
@@ -19,11 +19,10 @@
 
 # -d specify where to place the compiled class
 # --classpath path to externa jar and class
-deflib="hijack"
+
 debug=""
 echo "args $@"
-CLEAN=0
-while getopts ":dDPc" opt; do
+while getopts ":dDP" opt; do
   case $opt in
     P) pie="PIE=1"
       echo "position Indipendent code ENABLED"
@@ -31,36 +30,18 @@ while getopts ":dDPc" opt; do
     d) debug="DEBUG=1"
       echo "debug is on"
       ;;
-    c) CLEAN=1
-      echo "force clean is on"
-      ;;
     D) echo "debug is off"
       ;;
     \?) echo "Invalid option -$OPTARG"
       ;;
   esac
 done
-if [ $CLEAN -eq 0 ]
-then
-  echo "checking $(pwd)/libs/*/$deflib"
-  if [ -e $(pwd)/libs/*/$deflib ]
-  then
-    echo "nothing to do ... $deflib already present"
-    exit 0
-  fi
-fi
+
 cdir=$(pwd)
-if [ $CLEAN -eq 1 ]
-then
-  echo cleaning libs..
-  rm -r ./obj/* ./libs/*
-fi
+
+echo cleaning libs..
+rm -r ./obj/* ./libs/*
 ndk_cmd="ndk-build V=1 $debug $pie"
 echo "Building hijack $ndk_cmd"
-cd jni
-if [ $CLEAN -eq 1 ]
-then
-  ndk-build V=1 clean
-fi
-$ndk_cmd
+cd jni &&  ndk-build V=1 clean && $ndk_cmd
 cd ${cdir}
